5b5f38fe19a2223d5b576aabf43ebe128d2c65ff
yunisuthn <yunis.thn@gmail.com>
2021-05-26 14:08:34 +0300

    essaie

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const http = require('http');
-
-const extra_fs = require('fs-extra');
+   
+const path = require("path");
+const fs = require('fs');
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 8080;
 var dir_home = process.env[process.platform =="win32"?"USERPROFILE":"HOME"];
 
-var redacted_files_directory = require("path").join(dir_home, "Desktop","Redacted"); //variable pour le dossier a vider a chaque fois que le programme commence a traiter un dossier selectionné
-console.log("dir_home == ", redacted_files_directory);
+console.log("di_home == ", dir_home);
 
+// var redacted_files_directory = require("path").join(dir_home, "Desktop","Redacted"); //variable pour le dossier a vider a chaque fois que le programme commence a traiter un dossier selectionné
 // extra_fs.emptyDirSync(redacted_files_directory);
 
+fs.mkdirSync(path.join(dir_home,"Desktop", "Geeks"));
 const server = http.createServer((req, res) => {
     
 
